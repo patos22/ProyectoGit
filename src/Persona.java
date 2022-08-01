0192dbf301ff9c8bec78b2eb5f3b66ac3924a609
Aprendiente <Aprendiente@10.50.43.21>
2022-08-01 14:58:32 -0600

    Se agregan los metodos de acceso a la  clase persona

--- src/Persona.java
+++ src/Persona.java
@@ -10,5 +10,31 @@ public class Persona {
 private int codigo;
 private String nombre;
 private String direecion;
+// set y get
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireecion() {
+        return direecion;
+    }
+
+    public void setDireecion(String direecion) {
+        this.direecion = direecion;
+    }
+
     
 }
